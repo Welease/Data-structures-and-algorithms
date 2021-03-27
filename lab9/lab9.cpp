24b2daff80f5c78e757488a341c93065f8dd685f
Wynn Elease <tanzilya.farruhshina.01@mail.ru>
2021-03-27 20:26:16 +0300

    lab9 finished

--- lab9/lab9.cpp
+++ lab9/lab9.cpp
@@ -6,6 +6,11 @@
 #include <fcntl.h>
 #include "Tree.h"
 
+int getRandomNumber(int min, int max) {
+	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+	return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+
 void formTree(int n, TreeNode **parent, std::ifstream & file) {
 	TreeNode *now;
 	int x, nl, nr;
@@ -16,7 +21,7 @@ void formTree(int n, TreeNode **parent, std::ifstream & file) {
 	else {
 		nl = n / 2;
 		nr = n - nl - 1;
-		file >> x;
+		x = getRandomNumber(0, 99);
 		now = new TreeNode;
 		now->key = x;
 		formTree(nl, &now->left, file);
@@ -25,21 +30,21 @@ void formTree(int n, TreeNode **parent, std::ifstream & file) {
 	}
 }
 
-void printReversSim(TreeNode *root, int l) {
+void printReversSym(TreeNode *root, int l) {
 	if (root) {
-		printReversSim(root->right, l + 1);
+		printReversSym(root->right, l + 1);
 		for (int i = 1; i <= l; ++i) {
-			std::cout << "    ";
+			std::cout << "     ";
 		}
 		std::cout << root->key << std::endl;
-		printReversSim(root->left, l + 1);
+		printReversSym(root->left, l + 1);
 	}
 }
 
 void printInStraight(TreeNode *root, int l) {
 	if (root) {
 		for (int i = 1; i <= l; ++i) {
-			std::cout << "    ";
+			std::cout << "     ";
 		}
 		std::cout << root->key << std::endl;
 		printInStraight(root->left, l + 1);
@@ -48,25 +53,31 @@ void printInStraight(TreeNode *root, int l) {
 }
 
 
-void printInSim(TreeNode *root, int l) {
+void printInSym(TreeNode *root, int l) {
 	if (root) {
-		printInSim(root->left, l + 1);
+		printInSym(root->left, l + 1);
 		for (int i = 1; i <= l; ++i) {
-			std::cout << "    ";
+			std::cout << "     ";
 		}
 		std::cout << root->key << std::endl;
-		printInSim(root->right, l + 1);
+		printInSym(root->right, l + 1);
 	}
 }
 
 int main() {
 	std::ifstream file("/Users/welease/Data-structures-and-algorithms/lab9/input.txt");
 	TreeNode *root = nullptr;
-	int arr[21] = {8, 9, 11, 15, 19, 20, 21, 7, 3, 2, 1, 5, 6, 4, 13, 14, 10, 12, 17, 16, 18};
-	formTree(7, &root, file);
+	int n;
+	std::cout << "Input count of nodes:";
+	std::cin >> n;
+	formTree(n, &root, file);
+	std::cout << "in straight order:" << std::endl;
 	printInStraight(root, 0);
-	std::cout << "------------------" << std::endl;
-	printInSim(root, 0);
-	std::cout << "------------------" << std::endl;
-	printReversSim(root, 0);
+	std::cout << std::endl;
+	std::cout << "in straight symmetrical order:" << std::endl;
+	printInSym(root, 0);
+	std::cout << std::endl;
+
+	std::cout << "in reverse symmetrical order" << std::endl;
+	printReversSym(root, 0);
 }
